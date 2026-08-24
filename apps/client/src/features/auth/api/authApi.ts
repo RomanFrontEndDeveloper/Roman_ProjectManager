e4c500ec019301e12c43121d2c17f30e97b78636
RomanFronrEnd <romariotraveler@gmail.com>
2026-08-24 13:52:14 +0300

    create Frontend:  Feature-based Frontend Structure & API Layer (Axios)

---
+++ apps/client/src/features/auth/api/authApi.ts
@@ -0,0 +1,10 @@
+import { api } from "@/lib/axios";
+
+export const login = async (data: {
+  email: string;
+  password: string;
+}) => {
+  const response = await api.post("/auth/login", data);
+
+  return response.data;
+};
